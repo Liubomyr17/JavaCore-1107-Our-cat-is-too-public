35311973b5f4700da22a8c27391942b599418ced
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-28 09:05:43 +0200

    Add files via upload

---
+++ JavaCore 1107 Our cat is too public/src/com/company/Main.java
@@ -0,0 +1,88 @@
+package com.company;
+
+/*
+
+1107 Our cat is too public
+Hide all internal variables of the Cat class, but only those that can be accessed using methods.
+Requirements:
+1. The Cat variable name must be hidden.
+2. The age variable of the Cat class must be hidden.
+3. The Cat class weight variable must be open.
+4. There must be 3 variables in the Cat class.
+5. Cat must have private and public variables.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+    }
+    public class Cat {
+        private String name;
+        private int age;
+        public int weight;
+
+        public Cat(String name, int age, int weight) {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
